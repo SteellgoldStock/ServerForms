014954b5112604faf1bd5e4b7d891d9501114cb4
Gaëtan H <steellgold0@gmail.com>
2022-12-07 18:35:06 +0100

    Basic files

---
+++ src/steellgold/serverforms/SF.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace steellgold\serverforms;
+
+use pocketmine\plugin\PluginBase;
+
+class SF extends PluginBase {
+	protected function onEnable(): void {
+		// $this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
+	}
+
+	protected function onDisable(): void {
+
+	}
+}
