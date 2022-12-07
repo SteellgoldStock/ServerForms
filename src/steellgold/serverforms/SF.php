ecc42b3e2f86df2401af0b1907e21b30ea5db27b
Gaëtan H <steellgold0@gmail.com>
2022-12-07 21:11:11 +0100

    Instance and file

--- src/steellgold/serverforms/SF.php
+++ src/steellgold/serverforms/SF.php
@@ -3,13 +3,26 @@
 namespace steellgold\serverforms;
 
 use pocketmine\plugin\PluginBase;
+use steellgold\serverforms\listeners\PlayerListeners;
 
 class SF extends PluginBase {
+
+	public static $instance;
+
 	protected function onEnable(): void {
-		// $this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
+		self::$instance = $this;
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
 	}
 
 	protected function onDisable(): void {
 
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public static function getInstance() {
+		return self::$instance;
 	}
 }
